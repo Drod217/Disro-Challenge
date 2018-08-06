a5296227c78667bfa523ba415c1284707867b681
David Rodriguez <33043921+Drod217@users.noreply.github.com>
2018-08-05 23:50:42 -0500

    mobile changes

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+$(window).resize(function() {
+  location.reload();
+});
+
 $(document).ready(function() {
   $(".nav-funds").on("click", function() {
     balance = parseInt($(".balance").text());
     balance += 1.0;
     $(".balance").text(balance.toFixed(2));
   });
+});
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+$(".hamburger").click(function() {
+  $(".left-container").toggle();
+  $(".nav-funds").toggle();
+  $(".funds").toggle();
+  $(".nav-funds").css("height", "10vh");
 });
